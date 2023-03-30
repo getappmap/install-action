351435cd9b826a53f43e5d11cd33ad22a3e5ef79
Kevin Gilpin <kgilpin@gmail.com>
2023-03-30 09:28:57 -0400

    fix: Adjust download URL in smoke test for platform

--- test/smoke.spec.ts
+++ test/smoke.spec.ts
@@ -3,14 +3,15 @@ import {runAsScript} from '../src/index';
 
 const pwd = process.cwd();
 
+const platformName = process.platform === 'darwin' ? 'macos-arm64' : 'linux-x64';
+const scriptUrl = `https://github.com/getappmap/appmap-js/releases/download/%40appland%2Fappmap-preflight-v1.0-pre.1/appmap-preflight-${platformName}`;
+
 describe('install-appmap-action', () => {
   beforeEach(() => process.chdir(join(__dirname, 'fixture')));
   afterEach(() => process.chdir(pwd));
 
   it('installs AppMap tools', async () => {
-    const results = await runAsScript(
-      `https://github.com/getappmap/appmap-js/releases/download/%40appland%2Fappmap-preflight-v1.0-pre.1/appmap-preflight-macos-arm64`
-    );
+    const results = await runAsScript(scriptUrl);
     expect(results.patchFile).toBeTruthy();
   });
 });
